3ab8b9955cfb575d1d99afcea822b6e72507b233
Colin Aulds <caulds989@gmail.com>
2017-03-29 10:36:12 -0700

    no log

--- config.js
+++ config.js
@@ -2,4 +2,3 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                             'mongodb://localhost/gameswap';
 exports.PORT = process.env.PORT || 8080;
-console.log(exports);
